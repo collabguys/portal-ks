244be9a70fcf818fc89b8b54b2fc6d0714d9bcf9
felicita2031 <felicita2031@gmail.com>
2022-08-05 12:13:14 +0300

    yeni dosyalar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>deneme versiyon</h1>
+      <h2>kursat serdar</h2>
+    </div>
+  );
+}
+
+export default App;
